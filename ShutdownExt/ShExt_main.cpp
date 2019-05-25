d44a24ec42ffbc392bb61288e342ecf766420561
maximilionus <maksimka-up@yandex.ru>
2019-05-25 23:30:46 +0300

    Add project files.

---
+++ ShutdownExt/ShExt_main.cpp
@@ -0,0 +1,13 @@
+#include "ShExt_main.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThread]
+
+void main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	ShutdownExt::ShExt_main main_form;
+	Application::Run(%main_form);
+}
