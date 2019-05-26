c149c4c4dc16727a30f04ffc08bae0c8eb13545f
maximilionus <maksimka-up@yandex.ru>
2019-05-26 23:04:41 +0300

    Added Days:Hours:Mins:Sec UI elements and started with their fnc realisation

--- ShutdownExt/ShExt_FNCs.h
+++ ShutdownExt/ShExt_FNCs.h
@@ -1,1 +1,6 @@
 #pragma once
+int shext_convertToSeconds(int inputArr[3])
+{
+	int seconds = (inputArr[0] * 86400) + (inputArr[1] * 3600) + (inputArr[2] * 60) + inputArr[3];
+	return seconds;
+}
--- ShutdownExt/ShExt_main.h
+++ ShutdownExt/ShExt_main.h
@@ -15,6 +15,10 @@ namespace ShutdownExt {
 	public ref class ShExt_main : public System::Windows::Forms::Form
 	{
 	private: bool dragging;
+	private: System::Windows::Forms::TextBox^ tbox_In_Hours;
+	private: System::Windows::Forms::TextBox^ tbox_In_Mins;
+	private: System::Windows::Forms::TextBox^ tbox_In_Seconds;
+
 	private: Point offset;
 
 	public:
@@ -39,7 +43,9 @@ namespace ShutdownExt {
 		}
 	private: System::Windows::Forms::Button^ btn_run;
 	private: System::Windows::Forms::Button^ btn_stop;
-	private: System::Windows::Forms::TextBox^ tbox_timer_input;
+	private: System::Windows::Forms::TextBox^ tbox_In_Days;
+
+
 	private: System::Windows::Forms::Label^ label_Time;
 	private: System::Windows::Forms::Button^ btn_close;
 
@@ -65,16 +71,19 @@ namespace ShutdownExt {
 		{
 			this->btn_run = (gcnew System::Windows::Forms::Button());
 			this->btn_stop = (gcnew System::Windows::Forms::Button());
-			this->tbox_timer_input = (gcnew System::Windows::Forms::TextBox());
+			this->tbox_In_Days = (gcnew System::Windows::Forms::TextBox());
 			this->label_Time = (gcnew System::Windows::Forms::Label());
 			this->btn_close = (gcnew System::Windows::Forms::Button());
+			this->tbox_In_Hours = (gcnew System::Windows::Forms::TextBox());
+			this->tbox_In_Mins = (gcnew System::Windows::Forms::TextBox());
+			this->tbox_In_Seconds = (gcnew System::Windows::Forms::TextBox());
 			this->SuspendLayout();
 			// 
 			// btn_run
 			// 
-			this->btn_run->Location = System::Drawing::Point(12, 127);
+			this->btn_run->Location = System::Drawing::Point(12, 156);
 			this->btn_run->Name = L"btn_run";
-			this->btn_run->Size = System::Drawing::Size(260, 23);
+			this->btn_run->Size = System::Drawing::Size(105, 23);
 			this->btn_run->TabIndex = 0;
 			this->btn_run->Text = L"Run";
 			this->btn_run->UseVisualStyleBackColor = true;
@@ -83,20 +92,20 @@ namespace ShutdownExt {
 			// btn_stop
 			// 
 			this->btn_stop->Enabled = false;
-			this->btn_stop->Location = System::Drawing::Point(12, 156);
+			this->btn_stop->Location = System::Drawing::Point(292, 156);
 			this->btn_stop->Name = L"btn_stop";
-			this->btn_stop->Size = System::Drawing::Size(260, 23);
+			this->btn_stop->Size = System::Drawing::Size(105, 23);
 			this->btn_stop->TabIndex = 1;
 			this->btn_stop->Text = L"Stop";
 			this->btn_stop->UseVisualStyleBackColor = true;
 			this->btn_stop->Click += gcnew System::EventHandler(this, &ShExt_main::btn_stop_Click);
 			// 
-			// tbox_timer_input
+			// tbox_In_Days
 			// 
-			this->tbox_timer_input->Location = System::Drawing::Point(121, 29);
-			this->tbox_timer_input->Name = L"tbox_timer_input";
-			this->tbox_timer_input->Size = System::Drawing::Size(151, 20);
-			this->tbox_timer_input->TabIndex = 2;
+			this->tbox_In_Days->Location = System::Drawing::Point(162, 29);
+			this->tbox_In_Days->Name = L"tbox_In_Days";
+			this->tbox_In_Days->Size = System::Drawing::Size(25, 20);
+			this->tbox_In_Days->TabIndex = 2;
 			// 
 			// label_Time
 			// 
@@ -104,13 +113,13 @@ namespace ShutdownExt {
 			this->label_Time->ForeColor = System::Drawing::Color::White;
 			this->label_Time->Location = System::Drawing::Point(9, 32);
 			this->label_Time->Name = L"label_Time";
-			this->label_Time->Size = System::Drawing::Size(108, 13);
+			this->label_Time->Size = System::Drawing::Size(147, 13);
 			this->label_Time->TabIndex = 3;
-			this->label_Time->Text = L"Time to shutdown (H)";
+			this->label_Time->Text = L"Time to shutdown (D/H/M/S)";
 			// 
 			// btn_close
 			// 
-			this->btn_close->Location = System::Drawing::Point(266, 3);
+			this->btn_close->Location = System::Drawing::Point(377, 12);
 			this->btn_close->Name = L"btn_close";
 			this->btn_close->Size = System::Drawing::Size(20, 20);
 			this->btn_close->TabIndex = 4;
@@ -118,16 +127,40 @@ namespace ShutdownExt {
 			this->btn_close->UseVisualStyleBackColor = true;
 			this->btn_close->Click += gcnew System::EventHandler(this, &ShExt_main::Btn_close_Click);
 			// 
+			// tbox_In_Hours
+			// 
+			this->tbox_In_Hours->Location = System::Drawing::Point(193, 29);
+			this->tbox_In_Hours->Name = L"tbox_In_Hours";
+			this->tbox_In_Hours->Size = System::Drawing::Size(25, 20);
+			this->tbox_In_Hours->TabIndex = 2;
+			// 
+			// tbox_In_Mins
+			// 
+			this->tbox_In_Mins->Location = System::Drawing::Point(224, 29);
+			this->tbox_In_Mins->Name = L"tbox_In_Mins";
+			this->tbox_In_Mins->Size = System::Drawing::Size(25, 20);
+			this->tbox_In_Mins->TabIndex = 2;
+			// 
+			// tbox_In_Seconds
+			// 
+			this->tbox_In_Seconds->Location = System::Drawing::Point(255, 29);
+			this->tbox_In_Seconds->Name = L"tbox_In_Seconds";
+			this->tbox_In_Seconds->Size = System::Drawing::Size(25, 20);
+			this->tbox_In_Seconds->TabIndex = 2;
+			// 
 			// ShExt_main
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(30)), static_cast<System::Int32>(static_cast<System::Byte>(30)),
 				static_cast<System::Int32>(static_cast<System::Byte>(30)));
-			this->ClientSize = System::Drawing::Size(284, 191);
+			this->ClientSize = System::Drawing::Size(409, 191);
 			this->Controls->Add(this->btn_close);
 			this->Controls->Add(this->label_Time);
-			this->Controls->Add(this->tbox_timer_input);
+			this->Controls->Add(this->tbox_In_Seconds);
+			this->Controls->Add(this->tbox_In_Mins);
+			this->Controls->Add(this->tbox_In_Hours);
+			this->Controls->Add(this->tbox_In_Days);
 			this->Controls->Add(this->btn_stop);
 			this->Controls->Add(this->btn_run);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
@@ -162,10 +195,14 @@ namespace ShutdownExt {
 		this->dragging = false;
 	}
 	private: System::Void btn_run_Click(System::Object^ sender, System::EventArgs^ e) {
-		std::string* hours = new std::string;
-		system("shutdown -s -t 1800");
+		int* input_hours = new int[(Convert::ToInt32(this->tbox_In_Days->Text)), (Convert::ToInt32(this->tbox_In_Hours->Text)), (Convert::ToInt32(this->tbox_In_Mins->Text)), (Convert::ToInt32(this->tbox_In_Seconds->Text))];
+		const char* shext_seconds = (shext_convertToSeconds(input_hours));
+
+		system(shext_seconds);
 		this->btn_stop->Enabled = true;
 		this->btn_run->Enabled = false;
+
+		delete input_hours;
 	}
 	private: System::Void btn_stop_Click(System::Object^ sender, System::EventArgs^ e) {
 		system("shutdown -a");
